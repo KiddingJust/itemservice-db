cf9b021dd20132cfc7afffe2c6e8fffc0085d6a4
Kidding Just <rlarkdlr@gmail.com>
2022-06-07 20:45:57 +0900

    queryDSL

--- src/main/java/hello/itemservice/repository/jpa/JpaItemRepositoryV3.java
+++ src/main/java/hello/itemservice/repository/jpa/JpaItemRepositoryV3.java
@@ -1,6 +1,8 @@
 package hello.itemservice.repository.jpa;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import hello.itemservice.domain.Item;
 import hello.itemservice.domain.QItem;
@@ -14,6 +16,8 @@ import org.springframework.util.StringUtils;
 import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
+
+import static hello.itemservice.domain.QItem.item;
 
 @Repository
 @Transactional
@@ -54,20 +58,51 @@ public class JpaItemRepositoryV3 implements ItemRepository {
         String itemName = cond.getItemName();
         Integer maxPrice = cond.getMaxPrice();
 
+        //fetch를 하면 리스트로 나옴
+        //static import로 QItem 생략도 가능. QItem item = QItem.item;
+        List<Item> result = query
+                .select(item)
+                .from(item)
+                .where(likeItemName(itemName), maxPrice(maxPrice))
+                .fetch();
+
+        return result;
+    }
+
+    private BooleanExpression maxPrice(Integer maxPrice) {
+        if(maxPrice != null){
+            return item.price.loe(maxPrice);
+        }
+        return null;
+    }
+
+    private BooleanExpression likeItemName(String itemName){
+        if(StringUtils.hasText(itemName)){
+            return item.itemName.like("%"+itemName+"%");
+        }
+        return null;
+    }
+
+//    @Override
+    public List<Item> findAllOld(ItemSearchCond cond) {
+
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+
         //동적 쿼리 (itemName, maxPrice)
         BooleanBuilder builder = new BooleanBuilder();
         if(StringUtils.hasText(itemName)){
-            builder.and(QItem.item.itemName.like("%"+itemName+"%"));
+            builder.and(item.itemName.like("%"+itemName+"%"));
         }
         if(maxPrice != null){
-            builder.and(QItem.item.price.loe(maxPrice));
+            builder.and(item.price.loe(maxPrice));
         }
 
         //fetch를 하면 리스트로 나옴
         //static import로 QItem 생략도 가능. QItem item = QItem.item;
         List<Item> result = query
-                .select(QItem.item)
-                .from(QItem.item)
+                .select(item)
+                .from(item)
                 .where(builder)
                 .fetch();
 
