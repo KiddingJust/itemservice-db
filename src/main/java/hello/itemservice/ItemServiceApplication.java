b59ed6922f0f6bda1d7d6e5a6665248abbfa9f98
Kidding Just <rlarkdlr@gmail.com>
2022-06-07 21:24:12 +0900

    refactoring. spring data jpa & querydsl

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 
 //@Import(MemoryConfig.class)
-@Import(QuerydslConfig.class)
+@Import(V2Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
