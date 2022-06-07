565664f05bffe096cb8e58b0e47669a51ddc8ed0
Kidding Just <rlarkdlr@gmail.com>
2022-06-07 21:19:03 +0900

    refactoring. spring data jpa & querydsl

--- src/main/java/hello/itemservice/service/ItemServiceV2.java
+++ src/main/java/hello/itemservice/service/ItemServiceV2.java
@@ -7,12 +7,14 @@ import hello.itemservice.repository.v2.ItemQueryRepositoryV2;
 import hello.itemservice.repository.v2.ItemRepositoryV2;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ItemServiceV2 implements ItemService{
 
     private final ItemRepositoryV2 itemRepositoryV2;
@@ -25,16 +27,19 @@ public class ItemServiceV2 implements ItemService{
 
     @Override
     public void update(Long itemId, ItemUpdateDto updateParam) {
-
+        Item findItem = itemRepositoryV2.findById(itemId).orElseThrow();
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
     }
 
     @Override
     public Optional<Item> findById(Long id) {
-        return Optional.empty();
+        return itemRepositoryV2.findById(id);
     }
 
     @Override
-    public List<Item> findItems(ItemSearchCond itemSearch) {
-        return null;
+    public List<Item> findItems(ItemSearchCond cond) {
+        return itemQueryRepositoryV2.findAll(cond);
     }
 }
